4c71321935639703f7d0d11c7522a481246f02aa
Tiago Chacon <tiagochacon80@gmail.com>
2021-12-20 17:37:13 -0500

    update imc.js

--- imc.js
+++ imc.js
@@ -13,10 +13,28 @@ function imc() {
     if (nom !== '' && taille !== '' && poids !== '') {
         
         const resultatImc = (poids / (taille * taille)).toFixed(2);
-        result.textContent = resultatImc;
+
+        let classification = "";
+
+        if(resultatImc < 18.5) {
+            classification = "poids insuffisant";
+        }   if(resultatImc >= 18.5 && resultatImc <= 24.9){
+                classification = "poids normal";
+            }   if(resultatImc > 24.9 && resultatImc <= 29.9){
+                    classification = "excès de poids"
+                } if(resultatImc > 29.9 && resultatImc <= 34.9){
+                        classification = "obésité, classe I";
+                    }  if(resultatImc > 34.9 && resultatImc <= 39.9){
+                            classification = "obésité, classe II";
+                        } if(resultatImc > 39.9){
+                                classification = "obésité, classe III";
+                            }    
+
+       result.textContent = `${nom} votre ICM est ${resultatImc} et vous êtes ${classification}`;
+       
         
     }else {
-        result.textContent = 'Veuillez remplir tous les espaces vides!!!';
+        result.textContent = "Veuillez remplir tous les espaces vides!!!";
     }
 }
 
