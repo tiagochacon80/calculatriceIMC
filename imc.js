5bb0a98f4c608a3cb0d8214a60ced40362e306e2
Tiago Chacon <tiagochacon80@gmail.com>
2021-12-20 11:01:35 -0500

    update imc.js

--- imc.js
+++ imc.js
@@ -1,3 +1,24 @@
-<script>
-    
-</script>
+
+const calculer = document.getElementById('calculer');
+
+
+function imc() {
+    const femme = document.getElementById('femme');
+    const homme = document.getElementById('homme');
+    const nom = document.getElementById('nom').value;
+    const taille = document.getElementById('taille').value;
+    const poids = document.getElementById('poids').value; 
+    const result = document.getElementById('result');
+
+    if (nom !== '' && taille !== '' && poids !== '') {
+        
+        const resultatImc = (poids / (taille * taille)).toFixed(2);
+        result.textContent = resultatImc;
+        
+    }else {
+        result.textContent = 'Veuillez remplir tous les espaces vides!!!';
+    }
+}
+
+calculer.addEventListener('click', imc);
+
